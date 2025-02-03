a7e57bd3e46806722f9297024f8e579906e94cae
WhyNotYud <mtd.contactwithme@gmail.com>
2025-02-03 21:16:39 +0700

    feat(exception): add UserInvalidException for user validation errors

---
+++ src/main/java/com/website/jobfinder/exception/UserInvalidException.java
@@ -0,0 +1,7 @@
+package com.website.jobfinder.exception;
+
+public class UserInvalidException extends Exception {
+    public UserInvalidException(String message) {
+        super(message);
+    }
+}
